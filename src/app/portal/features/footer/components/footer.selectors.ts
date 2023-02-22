281a23809b06ffc266c7b2d24eed3ea3c1e4b78b
DOOMED2WIN <timon@codeschluss.de>
2023-02-22 12:00:44 +0100

    update

--- src/app/portal/features/footer/components/footer.selectors.ts
+++ src/app/portal/features/footer/components/footer.selectors.ts
@@ -1,10 +1,5 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { createFeatureSelector } from '@ngrx/store';
 import { footerFeatureKey } from '../constants/footer.constants';
 import { FooterState } from '../state/footer.reducer';
 
 export const selectFooterState = createFeatureSelector<FooterState>(footerFeatureKey);
-
-export const selectCurrentFooter = createSelector(
-  selectFooterState,
-  state => state.footer
-);
--- src/app/portal/features/footer/footer.module.ts
+++ src/app/portal/features/footer/footer.module.ts
@@ -4,15 +4,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { CoreModule } from 'src/app/core/core.module';
-import { PortalHeaderComponent } from '../../common/components/header/portal-header.component';
-import { PortalMenuComponent } from '../../common/components/menu/portal-menu.component';
+import { PortalFooterComponent } from './components/footer.component';
 import { footerFeatureKey } from './constants/footer.constants';
 import { FooterEffects } from './state/footer.effects';
 import { footerReducer } from './state/footer.reducer';
 
 const components: Type<any>[] = [
-  PortalHeaderComponent,
-  PortalMenuComponent,
+  PortalFooterComponent,
 ];
 
 const framework: Type<any>[] = [
