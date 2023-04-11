15897486ae381f9b575770aa18e48fb03853a9e1
vetemi <valmir@codeschluss.de>
2023-04-11 11:05:34 +0200

    little cleanup

--- src/app/core/animations/animations.ts
+++ src/app/core/animations/animations.ts
@@ -6,12 +6,12 @@ export const fadeInAnimation = trigger('fadeIn', [
     style({ opacity: 0, transform: 'scale(0.5)' }),
     animate('400ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))
   ]),
-])
+]);
 
 export const growOnSidesAnimation = trigger('growOnSides', [
-    transition(':enter', 
-    [
-      style({ opacity: 0, transform: 'scaleX(0)'}),
-      animate('400ms ease-out', style({ opacity: 1, transform: 'scaleX(1)'}))
-    ]),
-  ])
+  transition(':enter', 
+  [
+    style({ opacity: 0, transform: 'scaleX(0)'}),
+    animate('400ms ease-out', style({ opacity: 1, transform: 'scaleX(1)'}))
+  ]),
+]);
