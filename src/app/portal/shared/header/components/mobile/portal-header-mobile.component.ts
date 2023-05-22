06e41eb795ef045165c23317af04127c1d5089ce
PRiedmiller <peter@codeschluss.de>
2023-05-22 16:46:53 +0200

    User routing in menu

--- src/app/portal/shared/header/components/mobile/portal-header-mobile.component.ts
+++ src/app/portal/shared/header/components/mobile/portal-header-mobile.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { fadeInAnimation } from 'src/app/core/animations/animations';
 import { selectIsSearching } from 'src/app/portal/shared/search/state/search.selectors';
+import { Maybe, UserContextEntity } from 'src/schema/schema';
 
 @Component({
   selector: 'app-portal-header-mobile',
@@ -14,6 +15,9 @@ import { selectIsSearching } from 'src/app/portal/shared/search/state/search.sel
 export class PortalHeaderMobileComponent {
 
   public isSearching = this.store.select(selectIsSearching);
+
+  @Input()
+  public user?: Maybe<UserContextEntity>;
 
   constructor(
     private store: Store,
