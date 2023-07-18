45fae237c3fae7dec185cbe512fa40f9c6c874d5
vetemi <valmir@codeschluss.de>
2023-07-18 16:20:09 +0200

    571 - review changes

--- src/app/portal/features/map/page-feature/component/map-page-feature.component.ts
+++ src/app/portal/features/map/page-feature/component/map-page-feature.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Maybe } from 'graphql/jsutils/Maybe';
-import { combineLatest } from 'rxjs';
+import { Subject, combineLatest, takeUntil } from 'rxjs';
 import { MarkerDefinition } from 'src/app/shared/map/typings/map';
-import { DealEntity, EventEntity, OrganisationEntity } from 'src/schema/schema';
 import { MapPageFeatureActions } from '../state/map-page-feature.actions';
 import { selectSponsoredDeal, selectSponsoredEvent, selectSponsoredOrganisation } from '../state/map-page-feature.selectors';
 
@@ -12,48 +11,42 @@ import { selectSponsoredDeal, selectSponsoredEvent, selectSponsoredOrganisation 
   templateUrl: './map-page-feature.component.html',
   styleUrls: ['./map-page-feature.component.scss']
 })
-export class MapPageFeatureComponent implements OnInit{
+export class MapPageFeatureComponent implements OnDestroy {
   
-  public deal?: Maybe<DealEntity>;
+  public markers?: Maybe<MarkerDefinition[]>;
 
-  public event?: Maybe<EventEntity>;
-
-  public organisation?: Maybe<OrganisationEntity>;
-
-  public marker?: Maybe<MarkerDefinition[]>;
+  private destroy = new Subject<void>();
 
   constructor(
     private store: Store, 
-  ) { }
+  ) {
+    this.store.dispatch(MapPageFeatureActions.getSponsoredDeal());
+    this.store.dispatch(MapPageFeatureActions.getSponsoredEvent());
+    this.store.dispatch(MapPageFeatureActions.getSponsoredOrganisation());
 
-  public ngOnInit(): void {
+    combineLatest([
+      this.store.select(selectSponsoredEvent),
+      this.store.select(selectSponsoredOrganisation),
+      this.store.select(selectSponsoredDeal)
+    ]).pipe(takeUntil(this.destroy))
+      .subscribe(([event, organisation, deal]) => this.markers = [
+        {
+          entity: 'EventEntity',
+          data: [event]
+        },
+        {
+          entity: 'DealEntity',
+          data: [deal]
+        },
+        {
+          entity: 'OrganisationEntity',
+          data: [organisation]
+        }
+      ]);
+  }
 
-  this.store.dispatch(MapPageFeatureActions.getSponsoredDeal());
-  this.store.dispatch(MapPageFeatureActions.getSponsoredEvent());
-  this.store.dispatch(MapPageFeatureActions.getSponsoredOrganisation());
-
-  combineLatest([
-    this.store.select(selectSponsoredEvent),
-    this.store.select(selectSponsoredOrganisation),
-    this.store.select(selectSponsoredDeal)
-    ]).subscribe(([event, organisation, deal]) => {
-      this.event = event;
-      this.organisation = organisation;
-      this.deal = deal;
-
-      this.marker = [
-      {
-        entity: 'EventEntity',
-        data: [event]
-      },
-      {
-        entity: 'DealEntity',
-        data: [deal]
-      },
-      {
-        entity: 'OrganisationEntity',
-        data: [organisation]
-      }]
-   })
+  public ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
   }
 }
