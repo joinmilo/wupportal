23669747f040d63546cd339e5e8070560ef0ac3d
vetemi <valmir@codeschluss.de>
2023-09-26 08:01:58 +0200

    generic form stepper - wip

--- src/app/shared/form/stepper/components/step/form-step.component.ts
+++ src/app/shared/form/stepper/components/step/form-step.component.ts
@@ -33,7 +33,7 @@ export class FormStepComponent implements OnDestroy {
   public index?: number;
 
   public entered = false;
-  public touched = false;
+  public left = false;
 
   public resetValue?: unknown;
 
@@ -41,7 +41,27 @@ export class FormStepComponent implements OnDestroy {
 
   constructor(
     private store: Store,
-  ) {
+  ) { }
+
+  public register(index: number): void {
+    this.index = index;
+    this.resetValue = this.formGroup.value;
+    this.store.dispatch(FormStepperActions.registerStep(this.index, this.formGroup.status));
+    this.formGroup.valueChanges
+      .pipe(takeUntil(this.destroy))
+      .subscribe(() => this.store.dispatch(FormStepperActions.statusChanged(this.index as number, this.formGroup.status)))
+
+    this.initLastStep();
+    this.initCurrentStep();
+  }
+  
+  private initLastStep(): void {
+    this.store.select(selectLastStepIdx)
+      .pipe(takeUntil(this.destroy))
+      .subscribe(lastStep => this.lastStepIdx = lastStep);
+  }
+
+  private initCurrentStep(): void {
     this.store.select(selectCurrentStepIdx)
       .pipe(takeUntil(this.destroy))
       .subscribe(currentStepIdx => {
@@ -52,40 +72,17 @@ export class FormStepComponent implements OnDestroy {
         }
 
         if (this.entered && this.currentStepIdx !== this.index) {
-          this.touched = true;
+          this.left = true;
         }
       });
-
-    this.store.select(selectLastStepIdx)
-      .pipe(takeUntil(this.destroy))
-      .subscribe(lastStep => this.lastStepIdx = lastStep);
-  }
-
-  public register(index: number): void {
-    this.index = index;
-    this.resetValue = this.formGroup.value;
-    this.store.dispatch(FormStepperActions.registerStep(this.index, this.formGroup.status));
-    this.formGroup.valueChanges
-      .pipe(takeUntil(this.destroy))
-      .subscribe(() => this.store.dispatch(FormStepperActions.statusChanged(this.index as number, this.formGroup.status)))
   }
 
   public isInvalid(): boolean {
-    return this.touched && this.formGroup.invalid;
-  }
-
-  public reset(): void {
-    if (this.resetValue) {
-      this.formGroup.reset(this.resetValue);
-    }
+    return this.left && this.formGroup.invalid;
   }
 
   public setCurrentStep(): void {
     this.store.dispatch(FormStepperActions.setCurrentStepIdx((this.index || 0)));
-  }
-
-  public showBackButton(): boolean {
-    return this.index !== 0;
   }
 
   public back(): void {
@@ -96,8 +93,18 @@ export class FormStepComponent implements OnDestroy {
     this.store.dispatch(FormStepperActions.next());
   }
 
+  public showBackButton(): boolean {
+    return this.index !== 0;
+  }
+
   public showNextButton(): boolean {
     return this.index !== this.lastStepIdx;
+  }
+
+  public reset(): void {
+    if (this.resetValue) {
+      this.formGroup.reset(this.resetValue);
+    }
   }
 
   public ngOnDestroy(): void {
