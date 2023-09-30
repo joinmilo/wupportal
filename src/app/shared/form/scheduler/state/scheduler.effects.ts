1d03888b33267d3138cb22e83bccace9f3da9ca4
vetemi <valmir@codeschluss.de>
2023-09-30 18:09:49 +0200

    scheduler - wip

--- src/app/shared/form/scheduler/state/scheduler.effects.ts
+++ src/app/shared/form/scheduler/state/scheduler.effects.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { map } from 'rxjs';
+import { CoreActions } from 'src/app/core/state/actions/core.actions';
+import { FeedbackType } from 'src/app/core/typings/feedback';
+import { SchedulerActions } from './scheduler.actions';
 
 
 @Injectable()
 export class SchedulerEffects {
+
+  createdSuccessfully = createEffect(() => this.actions.pipe(
+    ofType(SchedulerActions.generateResult),
+    map(() => CoreActions.setFeedback({
+      type: FeedbackType.Success,
+      labelMessage: 'successfullyGeneratedSchedules',
+      labelAction: 'createNewSchedules'
+    })),
+  ));
 
   constructor(
     private actions: Actions,
