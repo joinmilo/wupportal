1659b7f479800b8c289f462138056752587ae4ce
doomed2code <doomed2code@doomed2code-ThinkPad-P51s>
2023-10-31 14:31:49 +0100

    dynamic title landing page

--- src/app/portal/modules/landing/component/portal-landing.component.ts
+++ src/app/portal/modules/landing/component/portal-landing.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Subject, takeUntil, tap } from 'rxjs';
 import { Maybe, MediaEntity, PageEntity } from 'src/app/core/api/generated/schema';
 import { PortalLandingActions } from '../state/portal-landing.actions';
 import { selectLandingPage } from '../state/portal-landing.selectors';
+import { TranslationService } from 'src/app/core/services/translation.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-portal-landing',
@@ -20,6 +22,8 @@ export class PortalLandingComponent implements OnInit, OnDestroy {
 
   constructor(
     private store: Store,
+    private translationService: TranslationService
+
   ) { }
 
   ngOnInit(): void {
@@ -31,7 +35,23 @@ export class PortalLandingComponent implements OnInit, OnDestroy {
     ).subscribe(page => {
       this.page = page;
       this.media = page?.uploads?.find(upload => upload?.title)?.media;
+
+      this.translationService.translatable(this.page?.translatables, 'name')
+        .pipe(takeUntil(this.destroy))
+        .subscribe(title => this.resize2fit(title));
     });
+  }
+  
+  private resize2fit(title: Maybe<string>): void {
+    const container = document.querySelector<HTMLElement>("#container");
+    if (!container) return;
+    container.children[0].textContent = title ?? null;
+    container.style.setProperty('font-size', "1em");
+    const {width, height} = container.children[0].getBoundingClientRect();
+    const {width: max_width, height: max_height} = container.getBoundingClientRect();
+    const fontSize = Math.min(max_width/width, max_height/height);
+    container.style.setProperty('font-size', fontSize + "em");
+    container.style.setProperty('letter-spacing', fontSize * 0.5 + "rem");
   }
 
   ngOnDestroy(): void {
