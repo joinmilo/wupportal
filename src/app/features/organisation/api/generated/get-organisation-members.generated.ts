d5c39fb91754050ba88f81dc2ed454fa48edccfd
PRiedmiller <peter@codeschluss.de>
2023-11-17 15:36:44 +0100

    organisation-admin-details-application invalid date fixed

--- src/app/features/organisation/api/generated/get-organisation-members.generated.ts
+++ src/app/features/organisation/api/generated/get-organisation-members.generated.ts
@@ -10,7 +10,7 @@ export type GetOrganisationMembersQueryVariables = Types.Exact<{
 }>;
 
 
-export type GetOrganisationMembersQuery = { __typename?: 'Query', getOrganisationMembers?: { __typename?: 'PageableList_OrganisationMemberEntity', total: any, result?: Array<{ __typename?: 'OrganisationMemberEntity', id?: string | null, approved?: boolean | null, isPublic?: boolean | null, userContext?: { __typename?: 'UserContextEntity', user?: { __typename?: 'UserEntity', id?: string | null, firstName?: string | null, lastName?: string | null, email?: string | null } | null } | null, organisation?: { __typename?: 'OrganisationEntity', id?: string | null, created?: any | null, modified?: any | null, slug?: string | null, sponsored?: boolean | null, name?: string | null, contact?: { __typename?: 'ContactEntity', id?: string | null, email?: string | null, name?: string | null, phone?: string | null, preferredContact?: boolean | null, website?: string | null } | null } | null } | null> | null } | null };
+export type GetOrganisationMembersQuery = { __typename?: 'Query', getOrganisationMembers?: { __typename?: 'PageableList_OrganisationMemberEntity', total: any, result?: Array<{ __typename?: 'OrganisationMemberEntity', id?: string | null, approved?: boolean | null, isPublic?: boolean | null, created?: any | null, modified?: any | null, userContext?: { __typename?: 'UserContextEntity', user?: { __typename?: 'UserEntity', id?: string | null, firstName?: string | null, lastName?: string | null, email?: string | null } | null } | null, organisation?: { __typename?: 'OrganisationEntity', id?: string | null, created?: any | null, modified?: any | null, slug?: string | null, sponsored?: boolean | null, name?: string | null, contact?: { __typename?: 'ContactEntity', id?: string | null, email?: string | null, name?: string | null, phone?: string | null, preferredContact?: boolean | null, website?: string | null } | null } | null } | null> | null } | null };
 
 export const GetOrganisationMembersDocument = gql`
     query getOrganisationMembers($params: FilterSortPaginateInput) {
@@ -19,6 +19,8 @@ export const GetOrganisationMembersDocument = gql`
       id
       approved
       isPublic
+      created
+      modified
       userContext {
         user {
           id
