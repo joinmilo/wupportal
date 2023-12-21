3b3c6766c3d5411187908589846097c0a7112f4f
vetemi <valmir@codeschluss.de>
2023-12-21 13:21:13 +0100

    fixed dynamic icon rendering

--- src/app/shared/widgets/icon/icon.component.ts
+++ src/app/shared/widgets/icon/icon.component.ts
@@ -1,28 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostBinding, Input, OnChanges } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { BrandIcon } from './typings/brand-icons';
 import { RegularIcon } from './typings/regular-icons';
 import { SolidIcon } from './typings/solid-icons';
 
-
 @Component({
   selector: 'app-icon',
-  templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss'],
+  template: ``,
   standalone: true,
   imports: [
     CommonModule,
   ]
 })
-export class IconComponent {
+export class IconComponent implements OnChanges {
 
   @Input({ required: true })
   public icon!: SolidIcon | RegularIcon | BrandIcon;
 
+  @HostBinding('innerHTML')
+  public renderedIconHTML?: SafeHtml;
+
   @Input()
   public size?: string = '1x';
-  
-  get iconClasses(): string {
-    return `${this.icon[0]} fa-${this.icon[1]} fa-${this.size}`;
+
+  constructor(
+    private el: ElementRef,
+    private cdr: ChangeDetectorRef,
+    private sanitizer: DomSanitizer,
+  ) {}
+
+  public ngOnChanges(): void {
+    const test = `<span class="${this.icon[0]} fa-${this.icon[1]} fa-${this.size}"></span> \n`
+    this.renderedIconHTML = this.sanitizer.bypassSecurityTrustHtml(test);
   }
 }
