60dfcf4666ae0532b9c8153fd400cfb32f14645f
PRiedmiller <peter@codeschluss.de>
2023-12-21 15:15:51 +0100

    styling share-dialog

--- src/app/shared/widgets/share/dialog/share-dialog.component.ts
+++ src/app/shared/widgets/share/dialog/share-dialog.component.ts
@@ -4,6 +4,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-share-dialog',
   templateUrl: 'share-dialog.component.html',
+  styleUrls: ['./share-dialog.component.scss'],
 })
 export class ShareDialogComponent {
 
