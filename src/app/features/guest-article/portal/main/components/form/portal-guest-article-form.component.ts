244642b9d203606f43e545841c3cc00d9510e8c5
DOOMED2WIN <timon-steinhauser@web.de>
2024-01-30 15:50:10 +0100

    fixed leftovers

--- src/app/features/guest-article/portal/main/components/form/portal-guest-article-form.component.ts
+++ src/app/features/guest-article/portal/main/components/form/portal-guest-article-form.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Maybe } from 'src/app/core/api/generated/schema';
 import { AppValidators } from 'src/app/core/validators/validators';
-import { selectArticleCategories } from 'src/app/features/article/admin/form/state/article-admin-form.selectors';
 import { PortalGuestArticleActions } from '../../state/portal-guest-article.actions';
 
 @Component({
@@ -11,12 +10,11 @@ import { PortalGuestArticleActions } from '../../state/portal-guest-article.acti
   templateUrl: './portal-guest-article-form.component.html',
   styleUrls: ['./portal-guest-article-form.component.scss'],
 })
-export class PortalGuestArticleFormComponent {
+export class PortalGuestArticleFormComponent{
 
   public contentForm = this.fb.group({
     id: ['' as Maybe<string>],
     name: ['' as Maybe<string>, [Validators.required]],
-    categoryId: [undefined as Maybe<string>],
     content: ['' as Maybe<string>],
   });
 
@@ -24,8 +22,6 @@ export class PortalGuestArticleFormComponent {
     userName: ['' as Maybe<string>, [Validators.required]],
     email: ['' as Maybe<string>, [Validators.required, AppValidators.email()]],
   });
-
-  public categories = this.store.select(selectArticleCategories);
 
   constructor(
     private store: Store,
@@ -41,9 +37,6 @@ export class PortalGuestArticleFormComponent {
       id: this.contentForm.value.id,
       captchaToken,
       name: this.contentForm.value.name,
-      category: this.contentForm.value.categoryId != null
-      ? { id: this.contentForm.value.categoryId }
-      : null,
       content: this.contentForm.value.content,
       publicAuthor: {
         name: this.infomationForm.value.userName,
