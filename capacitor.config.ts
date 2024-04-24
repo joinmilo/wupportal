4662d3711267f7a180b66069f8f1ca55e2d48b9a
vetemi <valmir@codeschluss.de>
2024-04-24 16:04:28 +0200

    TEG-531 - wip

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.joinmilo',
+  appName: 'milo',
+  webDir: 'dist/milo/browser'
+};
+
+export default config;
---
+++ src/environments/environment.mobile.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  base: 'https://demo.joinmilo.de/',
+  client: 'https://demo.joinmilo.de/',
+};
