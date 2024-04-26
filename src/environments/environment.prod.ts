4f50cabbde017d38e6581a7bfbcbc01067b39826
vetemi <valmir@codeschluss.de>
2024-04-26 16:18:53 +0200

    instance specific changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base: `${location.protocol}//${location.host}/`,
-  client: `${location.protocol}//${location.host}/`,
+  base: 'https://www.wupportal.org/',
+  client: 'https://www.wupportal.org/',
 };
